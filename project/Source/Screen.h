346496463e2f43ef3046affccb7ec7b62ea5d54a
ichikawa <aaa>
2026-09-04 13:41:51 +0900

    フルスクリーン

--- project/Source/Screen.h
+++ project/Source/Screen.h
@@ -8,7 +8,7 @@ namespace Screen
 {
 	static const int WIDTH = 1920;
 	static const int HEIGHT = 1080;
-	static const BOOL WINDOW_MODE = TRUE;
+	static const BOOL WINDOW_MODE = FALSE;
 	static const char* WINDOW_NAME = "project";
 	static const float WINDOW_EXTEND = 1.0f;
 	static const float FRAME_RATE = 60.0f;
